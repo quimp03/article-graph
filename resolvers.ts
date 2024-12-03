adaa293bf1cd16bd2c1f8b01ed2878848eb10dad
Mai Phuc Qui <maiphucqui8482@gmail.com>
2024-12-03 19:26:21 +0700

    CRUD in graphql - get data, create

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,27 @@
-export  const resolvers = {
+import Article from "./model/article.model"
+export const resolvers = {
     Query : {
-        hello: () => {
-            return "Hello World"
+        getListArticle: async () => {
+            const articles = await Article.find({
+                deleted: false
+            })
+            return articles
+        },
+        getArticle: async(_,args) => {
+           const {id} = args
+           const article = await Article.findOne({
+                _id: id,
+                deleted: false
+           })
+           return article
+        }
+    },
+    Mutation : {
+        createArticle: async (_, args) => {
+            const {article} = args
+            const newArticle = new Article(article)
+            await newArticle.save()
+            return newArticle
         }
     }
 }
--- typeDefs.ts
+++ typeDefs.ts
@@ -1,6 +1,23 @@
 import {gql } from "apollo-server-express"
-export const typeDefs = gql `
-type Query {
-    hello: String
-}
+export const typeDefs = gql`
+    type Article {
+        id: String,
+        title: String,
+        avatar: String,
+        description: String
+    }
+    type Query {
+        getListArticle: [Article]
+        getArticle(id: String): Article
+    }
+    
+    input ArticleInput{
+        title: String,
+        avatar: String,
+        description: String,
+    }
+    type Mutation {
+        createArticle(article: ArticleInput): Article
+    }
 `
+   
