edde9ede62435e02fe959520a3839266c7a14c10
Mai Phuc Qui <maiphucqui8482@gmail.com>
2024-12-03 15:02:20 +0700

    config GraphQL

--- index.ts
+++ index.ts
@@ -1,16 +1,26 @@
 import express, {Express, Request, Response} from "express"
 import env from "dotenv"
 import {connect} from "./config/database"
-import Article from "./model/article.model"
+import { ApolloServer, gql } from "apollo-server-express"
+import {typeDefs} from "./typeDefs"
+import {resolvers} from "./resolvers"
 env.config()
 const app: Express = express()
-const port: Number | String = process.env.PORT
+const port: (Number | String) = process.env.PORT
 connect()
-app.get("/", async(req: Request, res: Response) => {
-    const articles = await Article.find()
-    res.json(articles)
-})
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
-
+const startServer = async() => {
+    //GraphQl
+    const apolloServer = new ApolloServer({
+        typeDefs: typeDefs,
+        resolvers: resolvers
+    })
+    await apolloServer.start()
+    apolloServer.applyMiddleware({
+        app: app,
+        path: "/graphql",
+    })
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`)
+    })
+}
+startServer()
